6cfcb0cf2395eae28e03af04c6b4767a48ed2c97
DaryaYa <lyalyaka@mail.ru>
2020-04-12 22:49:41 +0300

    feat: switcher

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/extensions
 import './js/common.js';
+import './js/cards.js';
 // eslint-disable-next-line no-unused-vars
 import css from './css/main.css';
 import './scss/main.scss';
--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,23 @@
-const add = (a, b) => a + b;
-// eslint-disable-next-line no-console
-console.log(add(2, 6));
+
+const burger = document.querySelector('.header-burger');
+const headerNav = document.querySelector('.header-nav');
+const switcher = document.querySelector('.switcher');
+const checkbox = document.getElementById('switch-checkbox');
+
+let switchBar = true; // true = Game, false = Train  
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger-transform');
+    // TODO animash
+    headerNav.classList.toggle('nav-invisible');
+});
+
+switcher.addEventListener('change', () => {
+    console.log(switchBar);
+    if (checkbox.checked) {
+        headerNav.style.background = 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)';
+        switchBar = true;
+    } else {headerNav.style.background = 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(230,45,253,1) 100%)';
+    switchBar = false;
+    }
+});   
