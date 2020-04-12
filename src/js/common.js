7ab9da93dd7bf4e845569325ad24e80c07dd2d12
DaryaYa <lyalyaka@mail.ru>
2020-04-13 00:07:47 +0300

    refactor: switcher

--- src/js/common.js
+++ src/js/common.js
@@ -3,6 +3,7 @@ const burger = document.querySelector('.header-burger');
 const headerNav = document.querySelector('.header-nav');
 const switcher = document.querySelector('.switcher');
 const checkbox = document.getElementById('switch-checkbox');
+const switcherText = document.querySelector('.switch-label');
 
 let switchBar = true; // true = Game, false = Train  
 
@@ -16,8 +17,14 @@ switcher.addEventListener('change', () => {
     console.log(switchBar);
     if (checkbox.checked) {
         headerNav.style.background = 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)';
+        console.log(switcherText.dataset.on);
+        switcherText.textContent = switcherText.dataset.on;
+        switcherText.style.marginLeft = '-50px';
         switchBar = true;
     } else {headerNav.style.background = 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(230,45,253,1) 100%)';
+    console.log(switcherText.dataset.off);
+    switcherText.textContent = switcherText.dataset.off;
+    switcherText.style.marginLeft = '10px';
     switchBar = false;
     }
 });   
